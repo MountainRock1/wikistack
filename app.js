63752986c03302abbf00a0846cd3eca7c924aa1e
Alfred Meng <alfredfmeng@gmail.com>
2020-11-19 15:01:38 -0600

    working on models

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const layout = require('./views/layout')
+const { db } = require('./models');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send(layout(''))
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
